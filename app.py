f82a67a003b6884b65027062aa77d296957b5b8a
umemura-0678 <yume0678@gmail.com>
2025-09-06 10:52:20 +0900

    table items has no column named user_idのエラー

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, flash, redirect, render_template,request
+from flask import Flask, flash, redirect, render_template, request
 from flask_login import LoginManager, current_user, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from config import User,Message
+from config import User, Message, Item
 import time
 
 app = Flask(__name__)
@@ -55,7 +55,7 @@ def login():
         if user is not None and check_password_hash(user.password, request.form["password"]):
             login_user(user)
             flash(f"ようこそ！ {user.name} さん")
-            
+
             return redirect("/select")
 
         # NGならフラッシュメッセージを設定
@@ -82,12 +82,13 @@ def unregister():
     logout_user()
     return redirect("/")
 
- # メッセージ投稿フォームの表示・投稿
+
+# メッセージ投稿フォームの表示・投稿
 @app.route("/mess", methods=["GET", "POST"])
 def mess():
     if request.method == "POST" and current_user.is_authenticated:
         Message.create(user=current_user, content=request.form["content"])
-    
+
     messages = (
         Message.select()
         .where(Message.reply_to.is_null(True))
@@ -96,11 +97,33 @@ def mess():
     return render_template("/select_item/message.html", messages=messages)
 
 
+# 予約処理
+@app.route("/select/s1_video_camera", methods=["GET", "POST"])
+def reserve():
+    if request.method == "POST":
+        # データの検証
+        if not request.form["start_day"] or not request.form["end_day"]:
+            flash("未入力の項目があります。")
+            return redirect(request.url)
+
+        Item.create(
+            # item_name=request.form["item_name"],
+            item_name="ビデオカメラ",
+            start_date=request.form["start_day"],
+            end_date=request.form["end_day"],
+            # status=request.form["status"],
+            status="予約中",
+            user=current_user,
+        )
+        return render_template("/select.html")
+    return render_template("/select_item/s1_video_camera.html")
+
+
 # 管理画面ログイン機能
 @app.route("/admin_login", methods=["GET", "POST"])
 def admin_login():
     if request.method == "POST":
-        if not request.form["password"] :
+        if not request.form["password"]:
             flash("未入力の項目があります。")
             return redirect(request.url)
 
@@ -109,7 +132,7 @@ def admin_login():
         if user is not None and check_password_hash(user.password, request.form["password"]):
             login_user(user)
             flash(f"ようこそ！ {user.name} さん")
-            
+
             return redirect("/admin_menu")
 
         # NGならフラッシュメッセージを設定
@@ -117,77 +140,99 @@ def admin_login():
 
     return render_template("/admin/admin_login.html")
 
+
 @app.route("/admin_menu")
 def admin_menu():
     return render_template("/admin/admin_menu.html")
+
+
 # @app.route("/user_register")
 # def user_register():
 #     return render_template("/admin/user_register.html")
 @app.route("/user_delete")
 def user_delete():
     return render_template("/admin/user_delete.html")
+
+
 @app.route("/item_register")
 def item_register():
     return render_template("/admin/item_register.html")
+
+
 @app.route("/item_delete")
 def item_delete():
     return render_template("/admin/item_delete.html")
+
+
 @app.route("/mail_send")
 def mail_send():
     return render_template("/admin/mail_send.html")
+
+
 @app.route("/user_menu")
 def user_menu():
     return render_template("/admin/user_menu.html")
+
+
 @app.route("/item_menu")
 def item_menu():
     return render_template("/admin/item_menu.html")
-
-
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/select")
 def select():
     return render_template("select.html")
+
 
 @app.route("/select/s1_video_camera")
 def video_camera():
     return render_template("/select_item/s1_video_camera.html")
 
+
 @app.route("/select/s2_speaker")
 def speaker():
     return render_template("/select_item/s2_speaker.html")
+
 
 @app.route("/select/s3_video_deck")
 def video_deck():
     return render_template("/select_item/s3_video_deck.html")
 
+
 @app.route("/select/s4_tv")
 def tv():
     return render_template("/select_item/s4_tv.html")
+
 
 @app.route("/select/s5_refrigerator")
 def refrigerator():
     return render_template("/select_item/s5_refrigerator.html")
 
+
 @app.route("/select/s6_laundry_machine")
 def laundry_machine():
     return render_template("/select_item/s6_laundry_machine.html")
+
 
 @app.route("/select/s7_microwave")
 def microwave():
     return render_template("/select_item/s7_microwave.html")
 
+
 @app.route("/select/s8_rice_cooker")
 def rice_cooker():
     return render_template("/select_item/s8_rice_cooker.html")
 
+
 @app.route("/select/s9_hair_dryer")
 def hair_dryer():
     return render_template("/select_item/s9_hair_dryer.html")
+
 
 @app.route("/select/s10_air_conditioner")
 def air_conditioner():
--- config.py
+++ config.py
@@ -1,38 +1,42 @@
 import datetime
 from flask_login import UserMixin
 from peewee import SqliteDatabase, Model, IntegerField, CharField, TextField, TimestampField, ForeignKeyField
-   
+
 db = SqliteDatabase("db.sqlite")
- 
- 
+
 
 class User(UserMixin, Model):
-     id = IntegerField(primary_key=True)
-     name = CharField(unique=True)
-     email = CharField(unique=True)
-     password = TextField()
- 
-     class Meta:
-         database = db
-         table_name = "users"
- 
+    id = IntegerField(primary_key=True)
+    name = CharField(unique=True)
+    email = CharField(unique=True)
+    password = TextField()
+
+    class Meta:
+        database = db
+        table_name = "users"
+
+
 class Item(UserMixin, Model):
-     id = IntegerField(primary_key=True)
-     item_name = CharField(unique=True)
-     start_date = CharField(unique=True)
-     end_date = CharField(unique=True)
-     status = CharField(unique=True)
- 
-     class Meta:
-         database = db
-         table_name = "items"
+    id = IntegerField(primary_key=True)
+    item_name = CharField()
+    start_date = CharField()
+    end_date = CharField()
+    status = CharField()
+    user = ForeignKeyField(User, backref="items", on_delete="CASCADE")
+
+    class Meta:
+        database = db
+        table_name = "items"
+
 
 class Message(Model):
     id = IntegerField(primary_key=True)
     user = ForeignKeyField(User, backref="messages", on_delete="CASCADE")  # 参照先が削除された場合は削除する
     content = TextField()
     pub_date = TimestampField(default=datetime.datetime.now)
-    reply_to = ForeignKeyField("self", backref="messages", on_delete="CASCADE", null=True)  # 参照先が削除された場合は削除する nullを許容する
+    reply_to = ForeignKeyField(
+        "self", backref="messages", on_delete="CASCADE", null=True
+    )  # 参照先が削除された場合は削除する nullを許容する
 
     class Meta:
         database = db
@@ -40,7 +44,4 @@ class Message(Model):
 
 
 db.create_tables([User, Message, Item])
-db.pragma('foreign_keys', 1, permanent=True)  # on_deleteを動作させるオプション設定を追加
-
-
-
+db.pragma("foreign_keys", 1, permanent=True)  # on_deleteを動作させるオプション設定を追加
